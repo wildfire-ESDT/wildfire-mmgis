03a5cee4b42d98233bc17cba0d5a3f9e9556b8d8
Mohamad Saleh <mohamad.saleh@jpl.nasa.gov>
2026-06-08 10:19:00 -0700

    Adding dirs and routing to be used to interface with cmr.

---
+++ API/Backend/Cmr/setup.js
@@ -0,0 +1,19 @@
+const router = require("./routes/cmr");
+let setup = {
+  //Once the app initializes
+  onceInit: (s) => {
+    s.app.use(
+      s.ROOT_PATH + "/api/cmr",
+      s.ensureAdmin(),
+      s.checkHeadersCodeInjection,
+      s.setContentType,
+      router
+    );
+  },
+  //Once the server starts
+  onceStarted: (s) => {},
+  //Once all tables sync
+  onceSynced: (s) => {},
+};
+
+module.exports = setup;
